06fc7baf7c411a16f805e220f2a86a6d9d8c7a46
venkat954262 <venkat99mtm@gmail.com>
2021-04-21 11:43:17 +0530

    cronatb

--- runninglogpermission.sh
+++ runninglogpermission.sh
@@ -1,3 +1,8 @@
  chmod 655 /API-logs/access.log
  chmod 655 /API-logs/error.log
 
+crontab -e
+===========
+30     3       *       *       *       sh /home/apparao_it/permission.sh
+00      7       *       *       *       sh /home/apparao_it/runninglogpermission.sh
+31    3       *       *       *       sh Logsbkp.sh
