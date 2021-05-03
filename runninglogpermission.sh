b49b73f7c713c60860f09b02457c5921059a268a
venkat954262 <venkat99mtm@gmail.com>
2021-05-03 10:46:12 +0530

    Update runninglogpermission.sh

--- runninglogpermission.sh
+++ runninglogpermission.sh
@@ -5,4 +5,4 @@ crontab -e
 ===========
 30     3       *       *       *       sh /home/apparao_it/permission.sh
 00      7       *       *       *       sh /home/apparao_it/runninglogpermission.sh
-31    3       *       *       *       sh Logsbkp.sh
+31    3       *       *       *       sh /home/apparao_it/Logsbkp.sh
